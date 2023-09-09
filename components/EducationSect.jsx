46638d4ad309b44b031744e07dbb019632a6c3a6
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-09-09 10:51:02 -0300

    [feat] create 2 site sections

---
+++ components/EducationSect.jsx
@@ -0,0 +1,42 @@
+import { Image, Text, Flex, Divider } from '@chakra-ui/react'
+
+export default function EducationDevSect() {
+    return (
+        <Flex maxW='1120px' margin='auto' height='100vh' justifyContent='center' mt='10' flexDir='column' gap='10' >
+            <Flex alignItems='center' gap={4} justifyContent='center' >
+                <Image src='potion_red.svg' />
+                <Text color='white' fontSize='2xl' fontWeight='bold'>Education</Text>
+                <Image src='book_image.svg' />
+            </Flex>
+            <Flex width='100%' gap='5rem' >
+                <Flex flexDirection='column' maxW='600' border='1px solid white' padding='4' borderRadius='md' margin='auto' >
+                    <Flex justifyContent='space-between' alignItems='center' mb={4}  >
+                        <Text fontSize='xl' color='white'>Content Creation</Text>
+                        <Image src='' />
+                    </Flex>
+                    <Divider mb='4' />
+                    <Text color='white'>Wizardd develops comprehensive educational content that covers the basics of blockchain, the importance of Layer 2 solutions, NFTs, tokens, DeFi, and detailed guides on rollups. This content will be available in the form of articles, webinars, and free courses.</Text>
+                </Flex>
+            </Flex>
+            <Flex gap='5rem' >
+                <Flex flexDirection='column' maxW='600' border='1px solid white' padding='4' borderRadius='md' flex='1' >
+                    <Flex justifyContent='space-between' alignItems='center' mb={4} >
+                        <Text fontSize='xl' color='white'>Workshops and Training</Text>
+                        <Image src='' />
+                    </Flex>
+                    <Divider mb='4' />
+                    <Text color='white'>The company organizes workshops and training sessions for developers, companies, enthusiasts, and students from public and private universities who want to understand and work with Layer 2 rollups, NFTs, tokens, and DeFi.</Text>
+                </Flex>
+                <Flex flexDirection='column' maxW='600' border='1px solid white' padding='4' borderRadius='md' flex='1' >
+                    <Flex justifyContent='space-between' alignItems='center' mb={4} >
+                        <Text fontSize='xl' color='white'>Partnerships with Universities</Text>
+                        <Image src='' />
+                    </Flex>
+                    <Divider mb='4' />
+                    <Text color='white'>Wizardd intends to establish partnerships with public and private universities to offer education and training programs in blockchain, Layer 2 networks, tokens, and DeFi. This may include offering scholarships, organizing hackathons, and creating research and development laboratories.</Text>
+                </Flex>
+            </Flex>
+
+        </Flex>
+    )
+}
---
+++ components/SecuritySec.jsx
@@ -0,0 +1,42 @@
+import { Image, Text, Flex, Divider } from '@chakra-ui/react'
+
+export default function SecuritySec() {
+    return (
+        <Flex maxW='1120px' margin='auto' height='100vh' justifyContent='center' mt='10' flexDir='column' gap='10' >
+            <Flex alignItems='center' gap={4} justifyContent='center' >
+                <Image src='Pink_Potion.svg' />
+                <Text color='white' fontSize='2xl' fontWeight='bold'>Security</Text>
+                <Image src='securityImage.svg' />
+            </Flex>
+            <Flex width='100%' gap='5rem' >
+                <Flex flexDirection='column' maxW='600' border='1px solid white' padding='4' borderRadius='md' margin='auto' >
+                    <Flex justifyContent='space-between' alignItems='center' mb={4}  >
+                        <Text fontSize='xl' color='white'>Security Audit</Text>
+                        <Image src='' />
+                    </Flex>
+                    <Divider mb='4' />
+                    <Text color='white'>Wizardd provides security audit services for smart contracts and De-Fi applications. This is crucial to ensure the safety of users' assets and the integrity of the applications.</Text>
+                </Flex>
+            </Flex>
+            <Flex gap='5rem' >
+                <Flex flexDirection='column' maxW='600' border='1px solid white' padding='4' borderRadius='md' flex='1' >
+                    <Flex justifyContent='space-between' alignItems='center' mb={4} >
+                        <Text fontSize='xl' color='white'>Audit Report</Text>
+                        <Image src='' />
+                    </Flex>
+                    <Divider mb='4' />
+                    <Text color='white'>After the audit is completed, Wizardd provides a detailed report that includes all identified vulnerabilities, their severity, recommendations for fixing them, and an overall assessment of the application's security.</Text>
+                </Flex>
+                <Flex flexDirection='column' maxW='600' border='1px solid white' padding='4' borderRadius='md' flex='1' >
+                    <Flex justifyContent='space-between' alignItems='center' mb={4} >
+                        <Text fontSize='xl' color='white'>Post-Audit Support</Text>
+                        <Image src='' />
+                    </Flex>
+                    <Divider mb='4' />
+                    <Text color='white'>Wizardd provides ongoing support after the audit to help developers implement the recommended fixes and ensure the application is fully secure before being launched.</Text>
+                </Flex>
+            </Flex>
+
+        </Flex>
+    )
+}
