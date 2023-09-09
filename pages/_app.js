59514cc70d1b6d3d6c74fe5a21f40c5ecdd12860
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-09-09 10:52:12 -0300

    [feat] render starts image on all website

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex, Box, Image } from '@chakra-ui/react';
+import Header from '../components/Header';
+import SocialLinks from '../components/SocialLinks';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+      <Flex
+        direction="column"
+        align="center"
+        justify="flex-start"
+        bgImage="url('/gradient.png')"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        h="3660px"
+        w="100%"
+        position="relative"
+        zIndex={0}
+        overflowX="hidden"
+      >
+        <Box zIndex={1}>
+          <Header />
+          <Component {...pageProps} />
+          <SocialLinks />
+        </Box>
+      </Flex>
+    </ChakraProvider >
   )
 }
 
