ab41b814353ca8dd44e7303440989533c55bd5f1
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-09-09 10:52:33 -0300

    [feat] render all components on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Flex, Image } from '@chakra-ui/react'
+import Presentation from '../components/Presentation'
+import About from '../components/About'
+import SoftwareDevSec from '../components/SoftwareDevSect'
+import EducationDevSect from '../components/EducationSect'
+import SecuritySec from '../components/SecuritySec'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Presentation />
+      <About />
+      <SoftwareDevSec />
+      <EducationDevSect />
+      <SecuritySec />
+      <Flex alignItems='center' justifyContent='center'>
+        <Image src='wizardd_logo.svg' />
+      </Flex>
+    </div>
   )
 }
